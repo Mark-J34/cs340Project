95bdef998a0ca2bbb47edbb3b6218e3f50cfab74
Mark Jensen <jensenm3@flip1.engr.oregonstate.edu>
2023-11-09 12:19:57 -0800

    transferring files

---
+++ main.js
@@ -0,0 +1,30 @@
+function browse() {
+    document.getElementById('show').style.display = 'block';
+    document.getElementById('editDisplay').style.display = 'none';
+}
+
+function edit() {
+    document.getElementById('show').style.display = 'none';
+    document.getElementById('editDisplay').style.display = 'block';
+    
+}
+
+function addGame() {
+    
+}
+
+function addDeveloper() {
+
+}
+
+function browseDevelopers() {
+
+}
+
+function editDeveloper() {
+    
+}
+
+function sortByRating() {
+    
+}
