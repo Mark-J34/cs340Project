971dd99b0ae755c080df849b0a9172ce06351379
Mark Jensen <jensenm3@flip3.engr.oregonstate.edu>
2023-11-14 08:34:04 -0800

    Updated Version

--- main.js
+++ main.js
@@ -1,30 +1,38 @@
-function browse() {
-    document.getElementById('show').style.display = 'block';
-    document.getElementById('editDisplay').style.display = 'none';
+function showform(dowhat) {
+    /*
+    * four DIVS: show, add, edit, delete
+    * this function sets one visible the others not
+    */
+    if (dowhat == 'add'){
+        document.getElementById('show').style.display = 'none';
+        /*document.getElementById('add').style.display = 'block';*/
+        document.getElementById('edit').style.display = 'none';
+        /*document.getElementById('delete').style.display = 'none';*/
+    }
+    else if (dowhat == 'edit'){
+        document.getElementById('show').style.display = 'none';
+        /*document.getElementById('add').style.display = 'none';*/
+        document.getElementById('edit').style.display = 'block';
+        /*document.getElementById('delete').style.display = 'none';*/
+    }
+    else if (dowhat == 'delete'){
+        document.getElementById('show').style.display = 'none';
+        /*document.getElementById('add').style.display = 'none';*/
+        document.getElementById('edit').style.display = 'none';
+        /*document.getElementById('delete').style.display = 'none';*/
+    }
+
+    else { //by default display show
+
+        document.getElementById('show').style.display = 'block';
+        /*document.getElementById('add').style.display = 'none';*/
+        document.getElementById('edit').style.display = 'none';
+        /*document.getElementById('delete').style.display = 'none';*/
+    }
 }
 
-function edit() {
-    document.getElementById('show').style.display = 'none';
-    document.getElementById('editDisplay').style.display = 'block';
-    
-}
-
-function addGame() {
-    
-}
-
-function addDeveloper() {
-
-}
-
-function browseDevelopers() {
-
-}
-
-function editDeveloper() {
-    
-}
-
-function sortByRating() {
-    
-}
+function browse() {showform("browse")}
+function addGame() {showform('add')}
+function editGame() {showform('edit')}
+function editDeveloper() {showform('edit')}
+function editGameOwner(pid) {showform('edit')}
